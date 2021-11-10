a59ab0019337697542fb34c3dcdbb4690f423806
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-11-10 15:13:09 +0530

    10-11-2021

--- java/com/oslac/firebase/MainActivity.java
+++ java/com/oslac/firebase/MainActivity.java
@@ -20,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,Signup.class));
             }
         });
+        Button signIn = findViewById(R.id.Signin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Signin.class));
+            }
+        });
 
     }
 }
---
+++ java/com/oslac/firebase/Signin.java
@@ -0,0 +1,52 @@
+package com.oslac.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Signin extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    ConstraintLayout constraintLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        Button submit = findViewById(R.id.submit);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+
+        constraintLayout = findViewById(R.id.view);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Signin(email.getText().toString(),password.getText().toString());
+
+            }
+        });
+    }
+    public void Signin(String email, String password){
+       mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+           @Override
+           public void onSuccess(AuthResult authResult) {
+               Snackbar.make(constraintLayout,"Successfully sign in",Snackbar.LENGTH_SHORT).show();
+           }
+       });
+
+
+    }
+}
